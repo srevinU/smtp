18bc32c9d6337cb1ab55fa14e1c74f220fc0b540
Jenkins <cedricsegurafr@gmail.com>
2024-07-16 16:04:46 +0200

    header param update

--- python/main.py
+++ python/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI, Body
 from Smtp_server import Smtp
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
+origins = os.environ['ORIGINS'].split(',')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Message(BaseModel):
     from_: str
@@ -10,5 +22,6 @@ class Message(BaseModel):
 
 @app.post("/email/")
 async def send_email(message: Message):
+    print(message)
     myserver = Smtp()
     return myserver.send_email(from_=message.from_ ,message=message.message)
