486472902a88f3f70e135072ef8d3a1b58192488
Jenkins <cedricsegurafr@gmail.com>
2024-07-30 17:56:53 +0200

    main.py refacto

--- python/main.py
+++ python/main.py
@@ -4,6 +4,9 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import load_dotenv
+import uvicorn
+from pathlib import Path
+
 
 app = FastAPI()
 load_dotenv()
@@ -27,3 +30,6 @@ async def send_email(message: Message):
     print(message)
     myserver = Smtp()
     return myserver.send_email(from_=message.from_ ,message=message.message)
+
+if __name__ == '__main__':
+    uvicorn.run(f"{Path(__file__).stem}:app", host="0.0.0.0", port=8000, reload=True)
